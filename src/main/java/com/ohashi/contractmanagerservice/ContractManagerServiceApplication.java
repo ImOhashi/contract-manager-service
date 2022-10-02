cf174335cf38c870397dc42bf581c42fc8820533
ImOhashi <leonardoohashi.mkt@gmail.com>
2022-10-02 14:38:20 -0300

    first commit

---
+++ src/main/java/com/ohashi/contractmanagerservice/ContractManagerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ohashi.contractmanagerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContractManagerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContractManagerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ohashi/contractmanagerservice/ContractManagerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohashi.contractmanagerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContractManagerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
