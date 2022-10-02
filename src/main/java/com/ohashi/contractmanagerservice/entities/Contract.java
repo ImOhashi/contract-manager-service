990756fe2142f31d28b019270ed5193980f43a88
ImOhashi <leonardoohashi.mkt@gmail.com>
2022-10-02 15:21:35 -0300

    add a contract entity

---
+++ src/main/java/com/ohashi/contractmanagerservice/entities/Contract.java
@@ -0,0 +1,29 @@
+package com.ohashi.contractmanagerservice.entities;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "contract")
+public class Contract {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    private String name;
+
+    private String clientName;
+
+    private LocalDate initDate;
+
+    private LocalDate endDate;
+
+    private boolean renewed;
+}
